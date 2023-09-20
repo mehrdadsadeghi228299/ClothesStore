93d4bea1c7da1e7e5af11e555e018f779d17e31b
mehrdadsadeghi228 <63063456+mehrdadsadeghi228@users.noreply.github.com>
2023-09-20 14:45:28 -0700

    Create manager.controller.js

---
+++ app/controllers/manage/manager.controller.js
@@ -0,0 +1,50 @@
+
+const Controller = require('../../base.Controller');
+
+class AdminManager extends Controller{
+
+    addAdmin(req,res,next){
+        try {
+            
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    DeleteAdmin(req,res,next){
+        try {
+            
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    VerifyAdmin(req,res,next){
+        try {
+            
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    loginAdmin(req,res,next){
+        try {
+            
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    changePasswordAdmin(req,res,next){
+        try {
+            
+        } catch (error) {
+            next(error)
+        }
+    }
+
+}
+
+module.exports={
+    AdminManager:new AdminManager
+}
