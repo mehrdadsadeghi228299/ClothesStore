d65cc0a66d5f92d8f995ce28a2039deea62a6f8a
mehrdadsadeghi228 <63063456+mehrdadsadeghi228@users.noreply.github.com>
2023-09-30 09:51:04 -0700

    + have bug

--- app/controllers/wishList/wishlist.controller.js
+++ app/controllers/wishList/wishlist.controller.js
@@ -43,7 +43,7 @@ class WishListClass extends Controller {
             const ListProduct = CheckBeForAdding.ListProduct;
             if(ListProduct){
                for (let index = 0; index <ListProduct.length; index++) {
-               if(ListProduct==id_products){
+               if(ListProduct[index]==id_products){
                 return res.status(HttpStatus.NOT_IMPLEMENTED).json({
                     statusCodes: HttpStatus.NOT_IMPLEMENTED,
                     where: location,
@@ -72,9 +72,42 @@ class WishListClass extends Controller {
             next(error)
         }
     }
-    DeleteWishlist(req, res, next) {
+    async DeleteOneProductsWishlist(req, res, next) {
         try {
-            
+            var location='WishListClass/DeleteOneProductsWishlist';
+            const {id} = req.user;
+            const {id_products} = req.body;
+            if(!id_products) createHttpError.NotImplemented('id Products cannot be empty ');
+            const checkProducts = await ProductModel.findById(id_products);
+            if(!checkProducts) createHttpError.NotImplemented('id Products is not Exist in the products ');
+            const CheckBeForAdding = await WishListModel.findOne({user_id:id});
+            const ListProduct = CheckBeForAdding.ListProduct;
+            if(ListProduct){
+               for (let index = 0; index <ListProduct.length; index++) {
+               if(ListProduct[index]==id_products){
+                return res.status(HttpStatus.NOT_IMPLEMENTED).json({
+                    statusCodes: HttpStatus.NOT_IMPLEMENTED,
+                    where: location,
+                    message: " products before Adding to "
+                });
+               }
+               } 
+            }else{
+            const resultQuery = await WishListModel.findOneAndUpdate({user_id:id},{
+                '$push':{ListProduct:id_products}
+            });
+            return res.status(HttpStatus.OK).json({
+                statusCodes: HttpStatus.OK,
+                where: location,
+                message: resultQuery
+            });
+            }
+
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+                statusCodes: HttpStatus.INTERNAL_SERVER_ERROR,
+                where: location,
+                message: " there was a problem the products id  is not right or server in issus"
+            });
 
         } catch (error) {
             next(error)
